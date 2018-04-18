c4556aa03899278ab8bf3489480b5d392c593daf
linzhank <linzhank@gmail.com>
2018-04-18 14:22:21 -0400

    specify output dir, add timing to tfrecord creation script

--- dataset_utils/create_pitch_tf_record.py
+++ dataset_utils/create_pitch_tf_record.py
@@ -11,13 +11,13 @@ where 'intent##' is the unique label associated with
 these images.
 
 The training data set consists of 9 sub-directories (i.e. labels)
-each containing 1080 PNG images for a total of 9720 PNG images.
+each containing 1620 PNG images for a total of 1458 0 PNG images.
 
-The evaluation data set consists of 9 sub-directories (i.e. labels)
-each containing 380 PNG images for a total of 3420 PNG images.
+The validation data set consists of 9 sub-directories (i.e. labels)
+each containing 540 PNG images for a total of 4860 PNG images.
 
 The testing data set consists of 9 sub-directories (i.e. labels)
-each containing 380 PNG images for a total of 3420 PNG images.
+each containing 540 PNG images for a total of 4860 PNG images.
 
 This TensorFlow script converts the training, evaluation and testing data into
 a sharded data set consisting of 16, 8 and 8 TFRecord files, respectively.
@@ -29,21 +29,21 @@ a sharded data set consisting of 16, 8 and 8 TFRecord files, respectively.
 
 and
 
-  validation_directory/validation-00-of-08
-  validation_directory/validation-01-of-08
+  validation_directory/validate-0001-of-0008
+  validation_directory/validate-0002-of-0008
   ...
-  validation_directory/validation-08-of-08
+  validation_directory/validate-0008-of-0008
 
 and
 
-  test_directory/test-00-of-08
-  test_directory/test-01-of-08
+  test_directory/test-0001-of-0008
+  test_directory/test-0002-of-0008
   ...
-  test_directory/test-08-of-08
+  test_directory/test-0008-of-0008
 
 
-Each training TFRecord file contains ~608 records. Each validation TFRecord 
-file contains ~428 records. Each testing TFRecord file contains ~428 records. 
+Each training TFRecord file contains ~911 records. Each validation TFRecord 
+file contains ~608 records. Each testing TFRecord file contains ~608 records. 
 Each record within the TFRecord file is a serialized Example proto. 
 The Example proto contains the following fields:
 
@@ -61,17 +61,16 @@ The Example proto contains the following fields:
   image/class/pid: string specifying the unique ID of the pitcher,
     e.g. 'ZL'
 
-Running this script using 8 threads may take around ~? hours 
-on an Alienware Aurora R6.
+Running this script may take around 863 seconds on an Alienware Aurora R6.
 
-Running this script using 16 threads may take around ~? hours 
-on an Threadripper 1900X.
+Running this script may take around ? seconds on an Threadripper 1900X.
 """
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from datetime import datetime
+import time
 import os
 import glob
 import random
@@ -83,13 +82,14 @@ import tensorflow as tf
 
 import pdb
 
-#DATA_DIR = '/media/linzhank/DATA/Works/Intention_Prediction/Dataset/Ball pitch/pit2d9blk/'
-DATA_DIR = '/media/linzhank/850EVO_1T/Works/Data/Ball pitch/pit2d9blk/'
+DATA_DIR = '/media/linzhank/DATA/Works/Intention_Prediction/Dataset/Ball pitch/pit2d9blk'
+# DATA_DIR = '/media/linzhank/850EVO_1T/Works/Data/Ball pitch/pit2d9blk'
+TODAY = datetime.today().strftime("%Y%m%d")
 
-tf.app.flags.DEFINE_string('data_dir', DATA_DIR,
-                           'data directory')
-tf.app.flags.DEFINE_string('output_directory',  DATA_DIR+'tfrecord/',
-                           'Output data directory')
+tf.app.flags.DEFINE_string('data_dir', DATA_DIR, 'The original dataset lies here')
+tf.app.flags.DEFINE_string('output_directory',
+                           os.path.join(DATA_DIR, 'tfrecord_'+TODAY),
+                           'Generated TFRecords shards goes to here')
 
 tf.app.flags.DEFINE_integer('train_shards', 16,
                             'Number of shards in training TFRecord files.')
@@ -206,6 +206,12 @@ def process_image_files_shards(name, coder, ranges, annotations, num_shards):
     # Generate a sharded version of the file name, e.g. 'train-0002-of-0016'
     output_filename = "{}-{:05d}-of-{:05d}".format(name, s+1, num_shards)
     output_file = os.path.join(FLAGS.output_directory, output_filename)
+    if not os.path.exists(FLAGS.output_directory):
+      try:
+        os.makedirs(FLAGS.output_directory)
+      except OSError as exc: # Guard against race condition
+        if exc.errno != errno.EEXIST:
+          raise
     writer = tf.python_io.TFRecordWriter(output_file)
 
     indices_in_shard = np.arange(ranges[s][0], ranges[s][1], dtype=int)
@@ -225,14 +231,14 @@ def process_image_files_shards(name, coder, ranges, annotations, num_shards):
       writer.write(example.SerializeToString())
       counter += 1
 
-      print("{}: [shard-{:d}]: Processed {:d} of {:d} images in shard.".format
-              (datetime.now(), s+1, counter, len(indices_in_shard)))
+      print("{}: [{} shard-{:d}]: Processed {:d} of {:d} images in shard.".format
+              (datetime.now(), name, s+1, counter, len(indices_in_shard)))
       sys.stdout.flush()
 
     writer.close()
     shard_counter += 1
-    print("===> {}: [shard-{:d}] wrote to {}".format
-          (datetime.now(), shard_counter, output_file))
+    print("===> {}: [{} shard-{:d}] wrote to {}".format
+          (datetime.now(), name, shard_counter, output_file))
     sys.stdout.flush()
 
 
@@ -264,12 +270,17 @@ def process_annotated_files(name, annotations, num_shards):
 
   print('{}: Finished writing all {} images in {} dataset.'.format(datetime.now(),
                                                                    len(annotations[keys[0]]),
-                                                                   name+"ing"))
+                                                                   name))
   sys.stdout.flush()
 
 def read_annotation_files(name, directory):
-  files_location = os.path.join(directory,
-                                'dataset_config', 'travaltes_20180417', name+'*.txt')
+  """Read annotated and sliced dataset information files
+  Args: 
+    name: string, locates the dataset storage directory.
+    directory: string, root path to the data set.
+  """
+  files_dir = glob.glob(os.path.join(directory, 'dataset_config', '*'))[-1] # pick the newest files
+  files_location = os.path.join(files_dir, name+'*.txt')
   info_files = sorted(glob.glob(files_location))
   assert info_files, "No file was loaded!" # check if files are loaded
   annotations = {}
@@ -288,7 +299,7 @@ def process_dataset(name, directory, num_shards):
   """Process a complete data set and save it as a TFRecord.
 
   Args:
-    name: string, unique identifier specifying the data set (train, validate, test).
+    name: string, specifies dataset storing location.
     directory: string, root path to the data set.
     num_shards: integer number of shards for this data set.
   """
@@ -296,13 +307,14 @@ def process_dataset(name, directory, num_shards):
   process_annotated_files(name, annotations, num_shards)
 
 def main(unused_argv):
+  start_time = time.time()
   print('Saving results to %s' % FLAGS.output_directory)
 
   # Run it!
   process_dataset('train', FLAGS.data_dir, FLAGS.train_shards)
   process_dataset('validate', FLAGS.data_dir, FLAGS.validate_shards)
   process_dataset('test', FLAGS.data_dir, FLAGS.test_shards)
-
-
+  end_time = time.time()
+  print("It took {:g} seconds to create all tfrecord files".format(end_time - start_time))
 if __name__ == '__main__':
   tf.app.run()
